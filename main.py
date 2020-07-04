cc58046c36fbcb91cddd272898f84f87d5671091
Artanidos <artanidos@gmail.com>
2020-07-04 10:20:06 +0100

    upd

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+#############################################################################
+# Copyright (C) 2020 Olaf Japp
+#
+# This file is part of 13MoonCalendar.
+#
+#  13MoonCalendar is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  13MoonCalendar is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with 13MoonCalendar.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
 import os
+import sys
 import datetime  
 from weasyprint import HTML, CSS
 
@@ -12,13 +33,13 @@ def moon(moon, year):
     html += "<table class=\"moon\">\n"
     html += "<thead>\n"
     html += "<tr>\n"
-    html += "<th width=\"70\">Dali (Fr)</th>\n"
-    html += "<th width=\"70\">Seli (Sa)</th>\n"
-    html += "<th width=\"70\">Gamma (So)</th>\n"
-    html += "<th width=\"70\">Kali (Mo)</th>\n"
-    html += "<th width=\"70\">Alpha (Di)</th>\n"
-    html += "<th width=\"70\">Limi (Mi)</th>\n"
-    html += "<th width=\"70\">Silio (Do)</th>\n"
+    html += "<th width=\"70\">Dali</th>\n"
+    html += "<th width=\"70\">Seli</th>\n"
+    html += "<th width=\"70\">Gamma</th>\n"
+    html += "<th width=\"70\">Kali</th>\n"
+    html += "<th width=\"70\">Alpha</th>\n"
+    html += "<th width=\"70\">Limi</th>\n"
+    html += "<th width=\"70\">Silio</th>\n"
     html += "</tr>\n"
     html += "</thead>\n"
     html += "<tbody>\n"
@@ -50,19 +71,22 @@ def moon(moon, year):
     html += "</table>\n"
     return html
 
-year = 2020
-filename = "/media/art/data/SourceCode/13Moons/moons.pdf"
-html = "<html>\n<head>\n"
-html += "<link href=\"file:///media/art/data/SourceCode/13Moons/Creator/style.css\" rel=\"stylesheet\" type=\"text/css\"/>\n"
-html += "</head>\n<body>\n"
-html += "<p style=\"page-break-before: always\">"
-html += "<h1>13 Moon Calendar of the year " + str(year - 2019) + "</br>(" + str(year)+ ")</h1>"
+
+if __name__ == "__main__":
+    year = 2020
+    if len(sys.argv) == 2:
+        year = int(sys.argv[1])
+        
+    filename = "moons_" + str(year) + ".pdf"
+    html = "<html>\n<head>\n"
+    html += "<link href=\"file:///media/art/data/SourceCode/13Moons/Creator/style.css\" rel=\"stylesheet\" type=\"text/css\"/>\n"
+    html += "</head>\n<body>\n"
+    html += "<p style=\"page-break-before: always\">"
+    html += "<h1>13 Moon Calendar of the year " + str(year - 2019) + "</br>(" + str(year)+ ")</h1>"
     
-for m in range(1,14):
-    html += moon(m, year)
-html += "\n</body>\n</html>"
-h = HTML(string=html)
-css = CSS(string='@page { size: A5; margin: 0cm }')
-h.write_pdf(filename, stylesheets=[css])
-
-
+    for m in range(1,14):
+        html += moon(m, year)
+    html += "\n</body>\n</html>"
+    h = HTML(string=html)
+    css = CSS(string='@page { size: A5; margin: 0cm }')
+    h.write_pdf(filename, stylesheets=[css])
